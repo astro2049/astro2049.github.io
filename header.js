bc212d20960abed6ce202d23193fecd531eff02b
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2023-12-18 18:57:48 +0800

    tweaks

--- header.js
+++ header.js
@@ -1,13 +1,13 @@
 function addHeader() {
     const headerHtml =
         '<div style="padding: 30px 0 18px; display: flex; justify-content: space-between; align-items: center">' +
-        '<a style="width: 15%;" class="planets" href="index.html">Home</a>' +
-        '<a class="planets" href="atlas.html">Hourglass Twins</a>' +
-        '<a class="planets" href="Summer%20Wine.html">Brittle Hollow</a>' +
-        '<a class="planets" href="HVN%20ON%20EARTH.html">Orbital Probe Cannon</a>' +
-        '<a class="planets" href="ethereal.html">Giant’s Deep</a>' +
-        '<a class="planets" href="Apex.html">Attlerock</a>' +
-        '<a class="planets" href="credits.html">Sun Station [Credits]</a>' +
+        '<a style="width: 15%;" href="index.html">Home</a>' +
+        '<a href="hobbies.html">Extracurriculars</a>' +
+        '<a href="Summer%20Wine.html">Gallery</a>' +
+        '<a href="HVN%20ON%20EARTH.html">Vocabulary</a>' +
+        '<a href="ethereal.html">Formulas</a>' +
+        '<a href="Apex.html">Gamer Highlights</a>' +
+        '<a href="credits.html">Credits</a>' +
         '</div>';
     document.getElementById("header").innerHTML += headerHtml;
     const links = document.querySelectorAll("#header a");
