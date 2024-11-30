65faaba3ee8e7ca4bb23c7b10ebe2315be7ca553
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-11-30 23:32:50 +0000

    my games & refactors/tweaks

--- header.js
+++ header.js
@@ -2,10 +2,10 @@ function addHeader() {
     const headerHtml =
         '<div style="padding: 30px 0 18px; display: flex; justify-content: space-between; align-items: center">' +
         '<a style="width: 15%;" href="index.html">Home</a>' +
+        '<a href="mygames.html">My Games</a>' +
         '<a href="hobbies.html">Extracurriculars</a>' +
-        '<a href="Summer%20Wine.html">Gallery</a>' +
-        '<a href="HVN%20ON%20EARTH.html">Vocabulary</a>' +
-        '<a href="ethereal.html">Formulas</a>' +
+        '<a href="vocabulary.html">Vocabulary</a>' +
+        '<a href="formulas.html">Formulas</a>' +
         '<a href="Apex.html">Gamer Highlights</a>' +
         '<a href="credits.html">Credits</a>' +
         '</div>';
