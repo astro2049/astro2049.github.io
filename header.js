5f22dcd69c18d18c71e673a0d77d9474516c04f2
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2025-01-07 15:31:09 +0800

    Breakpoint

---
+++ header.js
@@ -0,0 +1,27 @@
+function addHeader() {
+    const headerHtml =
+        '<div style="padding: 30px 0 18px; display: flex; justify-content: space-between; align-items: center">' +
+        '<a style="width: 15%;" href="index.html">Home</a>' +
+        '<a href="mygames.html">My Games</a>' +
+        '<a href="hobbies.html">Extracurriculars</a>' +
+        '<a href="vocabulary.html">Vocabulary</a>' +
+        '<a href="formulas.html">Formulas</a>' +
+        '<a href="Apex.html">Gamer Highlights</a>' +
+        '<a href="credits.html">Credits</a>' +
+        '</div>';
+    document.getElementById("header").innerHTML += headerHtml;
+    const links = document.querySelectorAll("#header a");
+    const currentUrl = window.location.pathname.split("/").pop();
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        if (i === 0 && currentUrl === "") {
+            link.style.pointerEvents = "none";
+            link.setAttribute('aria-disabled', 'true');
+            break;
+        }
+        if (link.getAttribute("href") === currentUrl) {
+            link.style.pointerEvents = "none";
+            break;
+        }
+    }
+}
